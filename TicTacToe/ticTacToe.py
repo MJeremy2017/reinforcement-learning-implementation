e45acb7594008bf2ac1308f81ab5fa0746013fc5
MJeremy2017 <zhangyue9306@gmail.com>
2019-05-19 14:55:06 +0800

    update ttt

--- TicTacToe/ticTacToe.py
+++ TicTacToe/ticTacToe.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
 
     st = State(p1, p2)
     print("training...")
-    st.play(20000)
+    st.play(50000)
 
     # play with human
     p1 = Player("computer", exp_rate=0)
